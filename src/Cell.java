ef3e653ab3f856755000a1a34715b73f07533054
Emil Nymann <emilnymann96@gmail.com>
2020-04-19 13:22:16 +0200

    Added Cell class

---
+++ src/Cell.java
@@ -0,0 +1,33 @@
+public class Cell {
+    private int livingNeighbours;
+    private boolean alive;
+
+    void update() {
+        if (alive) {
+            if (livingNeighbours < 2 || livingNeighbours > 3) {
+                alive = false;
+            }
+        }
+        else {
+            if (livingNeighbours == 3) {
+                alive = true;
+            }
+        }
+    }
+
+    public int getLivingNeighbours() {
+        return livingNeighbours;
+    }
+
+    public void setLivingNeighbours(int livingNeighbours) {
+        this.livingNeighbours = livingNeighbours;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
